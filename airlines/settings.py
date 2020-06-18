16017baafa41bd31cb706e880962b6e755da65ed
Manish <manishkumar2981999@gmail.com>
2020-06-18 09:36:48 +0530

    added new changes

--- airlines/settings.py
+++ airlines/settings.py
@@ -69,6 +69,10 @@ TEMPLATES = [
         },
     },
 ]
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_DIRS = (
+    os.path.join(SETTINGS_PATH, 'templates'),
+)
 
 WSGI_APPLICATION = 'airlines.wsgi.application'
 
